7762f90d5b3a293651f88cfbda8ecb37b33b8e95
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-04-17 23:00:36 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,3 @@
-
 const discord = require('discord.js'),
 client = new discord.Client();
 const token = (process.env.TOKEN);
@@ -8,7 +7,7 @@ client.login(token)
 
 client.on('guildMemberAdd', member => {
 let role = member.guild.roles.find("name","Chomeur");
-const channel = member.guild.channels.find("name", "accueil").send(`Bienvenue sur MineEmplois ${member.user} ! .`)
+const channel = member.guild.channels.find("name", "acceuil").send(`Bienvenue sur MineEmplois ${member.user} ! .`)
 member.addRole(role)
 });
 
