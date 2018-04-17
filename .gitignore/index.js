a1d24b8d346f77f77d01bb7ed3e9a60ac6d2919f
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-04-17 22:47:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,14 @@
 
+const discord = require('discord.js'),
+client = new discord.Client();
+const token = (process.env.TOKEN);
+var prefix = "!";
+
+client.login(token)
+
+client.on('guildMemberAdd', member => {
+let role = member.guild.roles.find("name","Chomeur");
+const channel = member.guild.channels.find("name", "accueil").send(`Bienvenue sur MineEmplois ${member.user} ! .`)
+member.addRole(role)
+});
+
