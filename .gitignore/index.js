566d41a9f6ee18bf1d414448a52c9811218bb72e
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-04-19 16:31:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,3 +11,30 @@ const channel = member.guild.channels.find("name", "acceuil").send(`Bienvenue su
 member.addRole(role)
 });
 
+if (message.content.toLocaleLowerCase().includes("!demande")) {
+if(message.author.bot) return;
+message.reply("ta demande a été prise en compte, tu vas être contacté par un recruteur.");
+message.delete()
+client.channels.get('436532485518655498').send({embed: demande})
+}		
+	
+const demande = {
+  "url": "",
+  "color": 2977512,
+  "timestamp": new Date(),
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/icons/435480025110806548/c36bdd0f7f791094feb222a7d859f0b3.png",
+    "text": ""
+  },
+  "thumbnail": {
+    "url": message.author.displayAvatarURL
+  },
+  "fields": [
+	{
+      "name": message.author.username + " a fait une demande d'emplois : ",
+      "value": message + "",
+      "inline": false
+    }
+  ]
+};
+
