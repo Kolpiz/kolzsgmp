75e591dab00d6b587aca0e8f93e077b4b6a3fc37
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-05-07 18:51:13 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,4 +10,22 @@ client.on('guildMemberAdd', member => {
 let role = member.guild.roles.find("name","Esclave du grand maître");
 const channel = member.guild.channels.find("name", "allishere").send(`Wesh ${member.user}, t'es maintenant l'esclave du grand maître.`)
 member.addRole(role)
+  
+var Empire = client.guilds.get(message.guild.id).roles.find("name", "Empire");
+var Révolution = client.guilds.get(message.guild.id).roles.find("name", "Révolution");
+
+if(message.content.toLocaleLowerCase() == ("/empire")){
+ message.channel.send(`Tu fais partis de l'empire, tu as choisis ton camp !`);
+ message.guild.member(message.author).addRole(Empire);
+ message.guild.member(message.author).removeRole(Révolution);
+ }
+
+ if(message.content.toLocaleLowerCase() == ("#princesse")){
+ message.channel.send(`Tu fais partis de la révoltuion ! `);
+ message.guild.member(message.author).addRole(Révolution);
+ message.guild.member(message.author).removeRole(Empire);
+ }
+  
+  
+  
 });
