e8d917a4fae2c5453e7a3e36a9a9d4978ca272fb
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-06-04 17:04:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,11 +14,7 @@ client.on("message", message => {
  let argument = message.content.split("/clear").slice(1)
 if(!argument) return message.channel.send("Merci d'indiquer de respecter le modèle suivant : ``/clear ( nombre de 0 à 100)``").then(m => m.delete(20000))
 var person = message.member.permissions
-function dot() {
+if (!message.member.hasPermission('MANAGE_ROLES'))
 message.channel.bulkDelete(argument);
-};
-function doNot() {
-message.channel.send("e").then(m => m.delete(20000));
-};
-person.has("MANAGE_MESSAGES") ? dot() : doNot();
+});
 
