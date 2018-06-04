5a4d9a5ff1b4c39ab71f1eaaf7561e66ff2ceda1
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-06-04 16:39:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,23 @@
+// Début du code
+const discord = require('discord.js'),
+client = new discord.Client();
+const token = (process.env.TOKEN);
+var prefix = "/"; // Préfix du bot
 
+
+// Pemière action du bot quand il s'allume 
+
+client.login(token)
+
+client.on("message", message => { 
+let argument = message.content.split("/clear").slice(1)
+if(!argument) return message.channel.send("Merci d'indiquer de respecter le modèle suivant : ``/clear ( nombre de 0 à 100)``").then(m => m.delete(20000))
+var person = message.member.permissions
+function dot() {
+message.channel.bulkDelete(argument);
+};
+function doNot() {
+message.channel.send(" ").then(m => m.delete(20000));
+};
+person.has("MANAGE_MESSAGES") ? dot() : doNot();
+ });
