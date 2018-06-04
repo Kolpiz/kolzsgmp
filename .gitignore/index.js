7e885a299f90479f027ff7b5841cd8f4de6cc654
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-06-04 17:00:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,14 +11,14 @@ client.login(token)
 
 client.on("message", message => { 
 
-let argument = message.content.split("/clear").slice(1)
+ let argument = message.content.split("/clear").slice(1)
+if(!argument) return message.channel.send("Merci d'indiquer de respecter le modèle suivant : ``/clear ( nombre de 0 à 100)``").then(m => m.delete(20000))
 var person = message.member.permissions
 function dot() {
 message.channel.bulkDelete(argument);
 };
 function doNot() {
-message.channel.send(" ").then(m => m.delete(20000));
+message.channel.send("e").then(m => m.delete(20000));
 };
 person.has("MANAGE_MESSAGES") ? dot() : doNot();
 
- });
