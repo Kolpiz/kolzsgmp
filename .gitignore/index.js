cfeddc9613f61af7617a94d2ab62b0f76f6ab606
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-06-22 21:10:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,31 @@
+// Début du code
+const discord = require('discord.js'),
+client = new discord.Client();
+const token = (process.env.TOKEN);
+var prefix = "/"; // Préfix du bot
+
+
+// Pemière action du bot quand il s'allume 
+
+client.on("message", message => {
+
+
+let roleID = "448818258968182795";
+let membersWithRole = message.guild.roles.get(roleID).members;
+
+if (message.content.includes("test")) {
+membersWithRole.forEach(member => { member.send('test') });
+}
+
+});
+
+client.login(token)
+
+
+
+
+
+
+
+
 
