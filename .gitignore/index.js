886ec2072d17c50a85dc5071e3bc6d239de47a42
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-11-16 16:01:05 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -33,19 +33,17 @@ message.guild.member(message.author).addRole(news);
 }
 	
 if (message.content == "!stop-news") {	
-	
-	let news = message.guild.roles.get("463878931423166484");
-	
 	if(message.member.roles.has(news)){
-		
+	
+		let news = message.guild.roles.get("463878931423166484");
+	
 		message.channel.send("Désolé du dérangement occasionné !\ n_La direction de Parks-Mc Community_")
 		
 	message.guild.member(message.author).removeRole(news);	
-	} 
-	
-	else {
 		
-	message.channel.send("Tu n'as pas le rôle News !")
+	} else {
+	
+		message.channel.send("Tu n'as pas le rôle News !")
 	}	
 }
 	
