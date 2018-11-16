689eb4be4f3e42a118b706bfeeb26402738865c9
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2018-11-16 16:16:42 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -27,17 +27,15 @@ client.on('message', message =>{
 if (message.content == "!news"){
 	
 var news = message.guild.roles.get("463878931423166484");
-message.channel.send("Merci d'avoir prit le grade News !\n \n_La direction de Parks-Mc Community_")
+message.author.send("Merci d'avoir prit le grade News !\n \n_La direction de Parks-Mc Community_")
 message.guild.member(message.author).addRole(news);
 	
 }
 	
 if (message.content == "!stop-news") {	
-	if(message.member.roles.has(news))	
-		message.channel.send("Désolé du dérangement occasionné !\n \n_La direction de Parks-Mc Community_")	
-		message.guild.member(message.author).removeRole(news);
-		
-	}
+	message.author.send("Désolé du dérangement occasionné !\n \n_La direction de Parks-Mc Community_")	
+	message.guild.member(message.author).removeRole(news);	
+}
 
 	
 if (message.content.startsWith(prefix + 'annonce')) {
