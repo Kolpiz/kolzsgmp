234fac396356e4e1ae1b551b841720a1ceb72588
Kolpiz <37116238+Kolpiz@users.noreply.github.com>
2019-03-23 16:57:46 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,7 +11,7 @@ client.on("ready", () => {
 client.login(token)
 
 client.on('guildMemberAdd', member => {
-const channel = member.guild.channels.find(channel => channel.name === "🖐-bienvenue").send(`Bienvenue ${member.user} sur **🔴🔷Parks-Mc Community 2.0🔷🔴**🎉🤗 !`)
+const channel = member.guild.channels.find(channel => channel.name === "🖐-bienvenue").send(`Bienvenue ${member.user} sur **🔴🔷Parks-Mc Community 2.0🔷🔴**🎉🤗 ! Nous sommes maintenant **${member.count}** !`)
 
        let guest = member.guild.roles.get("483255290108706816");
         member.addRole(guest).catch(console.error);
